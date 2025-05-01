b6a2e70541e0bb28fd69b09cca28c90fef41d5ad
Jack Duan <jduan@eagl.co>
2025-05-01 12:09:18 -0700

    removed seconds in time display. #13

--- scripts.js
+++ scripts.js
@@ -92,7 +92,13 @@ function renderTrackingData(data) {
       "absolute left-0 top-2 w-1.5 h-1.5 -translate-x-1/2 rounded-full bg-black";
     eventDiv.appendChild(dot);
 
-    const date = new Date(event.when).toLocaleString();
+    const date = new Date(event.when).toLocaleString(undefined, {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    });
     // const notes = event.notes ? `<div class="text-sm text-black/60 mt-2 italic">${event.notes}</div>` : '';
     const notes = event.notes
       ? `<div class="text-sm ${event.additional?.exceptionCode ? "text-red-600" : "text-black/60"} mt-2 italic">${event.notes}</div>`
